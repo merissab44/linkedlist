386f9b588f169b97293c1c9282bff0f991dfbcb5
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-05-02 19:37:04 -0700

    completedproblem 2

--- ll.py
+++ ll.py
@@ -6,3 +6,16 @@ def find_middle(head):
         left = left.next
         right = right.next.next
     return left.data
+
+# Given two sorted linked lists, merge them so that the resulting linked list is also sorted.
+def merge_sorted(head1, head2):
+    if not head1:
+        return head2
+    if not head2:
+        return head1
+    if head1.data < head2.data:
+        head1.next = merge_sorted(head1.next, head2)
+        return head1
+    else:
+        head2.next = merge_sorted(head1, head2.next)
+        return head2
