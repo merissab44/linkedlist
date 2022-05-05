6eeb7b05f12857449e4407b649fcb3a7b317386b
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-05-04 22:49:21 -0700

    completed homework problem 2

--- ll.py
+++ ll.py
@@ -167,4 +167,22 @@ def rearrange_ll(head):
         head1 = head2
         head2 = next_node
 
+def rotate_list(head, rotations):
+    if not head or not head.next: return head
+        
+    last, n = head, 1
+    while last.next:
+        last = last.next
+        n += 1
+            
+    if rotations % n == 0: return head
+        
+    middle = head
+    for i in range(n - rotations%n-1):
+        middle = middle.next
+            
+    new_head = middle.next
+    last.next = head
+    middle.next = None
+    return new_head
     
